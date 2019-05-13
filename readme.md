a9acc405804525586095b587ac698d02ab554e64
Elisa Biver <elisa.biver@heig-vd.com>
2019-05-13 13:48:20 +0200

    First commit in project

---
+++ readme.md
@@ -0,0 +1,36 @@
+# Projet fin de cours VisualDon
+
+
+## Les données
+**Où avez-vous trouvé les données ?**
+
+Les données ont été trouvées sur Kaggle.com et ont été téléchargées en format CSV.
+
+**Comment vous les avez transformées ?**
+
+Il était compliqué de les transformer car il y avait plus de 65'000 données, ce qui le rend très lourd. J'ai dû les trier, supprimer certaines données et choisir celles que je voulais. J'ai utilisé un convertisseur en ligne : https://www.freeformatter.com/csv-to-json-converter.html
+
+J'ai ensuite utilisé Chart.js pour mettre en forme mes données. 
+
+**Quels choix vous avez faits et pourquoi ?**
+
+J'ai choisi de faire un graphique pour pouvoir montrer des moyennes de manière plus faciles. J'aurais pu également choisir de cartographier car les données me le permettaient mais je trouvais plus intéressant de pouvoir voir la moyenne des notes de vins par pays.
+
+**Comment vous avez visualisé les données ?**
+
+J'ai choisi un graphique en barre. Il y a trois barres par pays, ce qui représente les vins bons marché, de prix intermédiaire et chers. J'ai donc divisé mes données dans ces trois catégories pour chaque pays que j'ai choisi de représenter. Ensuite, j'ai fait la moyenne des notes (qui est représentée par l'axe Y) pour chaque catégorie. J'ai également voulu représenter une moyenne de prix pour chaque pays pour voir s'il y avait de gros écarts entre chaque pays.
+
+**Une explication sur le choix du type de représentation :**
+
+Le graphique en barre était à mon sens le type le plus adapté à mes informations. Les bulles pour la moyenne des prix par pays permet de bien situer cette dernière tout en ayant l'axe Y adapté. 
+
+
+## But du projet
+
+**Ce que vous souhaitez démontrer**
+
+J'ai voulu choisir des pays un peu moins connus pour les vins, en excluant la France, l'Italie et l'Espagne. En choisissant l'Argentine, les USA, l'Afrique du Sud et le Chili, je souhaite démontrer qu'il n'y a pas uniquement les vins très populaires qui sont bons mais également ceux d'autres continents, ce qui peut permettre d'élargir nos horizons et découvrir de nouvelles choses.
+
+**Le public cible**
+
+Ce graphique cible principalement les amateurs de vins.
---
+++ src/index.js
@@ -0,0 +1,124 @@
+import Chart from "chart.js"; 
+
+const DATAS = require('../data/wine.json');
+
+const wine = DATAS.wine.map(d => ({
+    country: d.country,
+    price: d.price,
+    points: d.points
+})).filter(item => item.price);
+
+const IS_CHEAP = p => p.price <= 30;
+const IS_MED = p => p.price > 30 && p.price <=80;
+const IS_HIGH = p => p.price > 80;
+const ALL = p => p.price > 0;
+
+
+const SUM = (wines) => wines.map(d => d.points).reduce((res, p) => res + p, 0);
+const MOY = (wines) => SUM(wines)/wines.length;
+const PRICE_SUM = (wines) => wines.map(d => d.price).reduce((res, p) => res + p, 0);
+const PRICE_MOY = (wines) => PRICE_SUM(wines)/wines.length;
+
+const SELECT_COUNTRY_FILTRE = (country, filtre) => 
+    wine.filter(d => d.country === country).filter(filtre);
+
+
+const ARG_CHEAP = Math.round(MOY(SELECT_COUNTRY_FILTRE("Argentina", IS_CHEAP)));
+const ARG_MED = Math.round(MOY(SELECT_COUNTRY_FILTRE("Argentina", IS_MED)));
+const ARG_HIGH = Math.round(MOY(SELECT_COUNTRY_FILTRE("Argentina", IS_HIGH)));
+const ARG_PRICE = Math.round(PRICE_MOY(SELECT_COUNTRY_FILTRE("Argentina", ALL)));
+
+const USA_CHEAP = Math.round(MOY(SELECT_COUNTRY_FILTRE("US", IS_CHEAP)));
+const USA_MED = Math.round(MOY(SELECT_COUNTRY_FILTRE("US", IS_MED)));
+const USA_HIGH = Math.round(MOY(SELECT_COUNTRY_FILTRE("US", IS_HIGH)));
+const USA_PRICE = Math.round(PRICE_MOY(SELECT_COUNTRY_FILTRE("US", ALL)));
+
+const SA_CHEAP = Math.round(MOY(SELECT_COUNTRY_FILTRE("South Africa", IS_CHEAP)));
+const SA_MED = Math.round(MOY(SELECT_COUNTRY_FILTRE("South Africa", IS_MED)));
+const SA_HIGH = Math.round(MOY(SELECT_COUNTRY_FILTRE("South Africa", IS_HIGH)));
+const SA_PRICE = Math.round(PRICE_MOY(SELECT_COUNTRY_FILTRE("South Africa", ALL)));
+
+const CH_CHEAP = Math.round(MOY(SELECT_COUNTRY_FILTRE("Chile", IS_CHEAP)));
+const CH_MED = Math.round(MOY(SELECT_COUNTRY_FILTRE("Chile", IS_MED)));
+const CH_HIGH = Math.round(MOY(SELECT_COUNTRY_FILTRE("Chile", IS_HIGH)));
+const CH_PRICE = Math.round(PRICE_MOY(SELECT_COUNTRY_FILTRE("Chile", ALL)));
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+ctx.canvas.width = ctx.canvas.clientWidth;
+ctx.canvas.height = ctx.canvas.clientHeight/2;
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Agentina', 'USA', 'South Africa', 'Chile'],
+        datasets: [
+        {
+            type: 'bubble',
+            label: 'Price average ($)',
+            stack: 'Stack 0',
+            borderColor: 'rgba(214, 214, 214, 0.8)',
+            backgroundColor: 'rgba(214, 214, 214, 0.6)', 
+            hoverBackgroundColor: 'rgba((214, 214, 214)',
+            borderWidth: 2,
+            fill: false,
+            radius: 10,
+            data: [ARG_PRICE, USA_PRICE, SA_PRICE, CH_PRICE] 
+        },
+        {
+            label: '0-30$',
+            backgroundColor: 'rgba(148, 0, 42, 0.6)', 
+            hoverBackgroundColor: 'rgba(148, 0, 42, 0.8)',
+            stack: 'Stack 0',
+            data: [ARG_CHEAP, USA_CHEAP, SA_CHEAP, CH_CHEAP],
+            borderColor: 'rgba(148, 0, 42, 1)',
+            borderWidth: 1
+            
+        },
+        {
+            label: '31-80$',
+            backgroundColor: 'rgba(84, 0, 24, 0.8)',
+            hoverBackgroundColor: 'rgba(84, 0, 24, 1)',
+            stack: 'Stack 1',
+            data: [ARG_MED, USA_MED, SA_MED, CH_MED],
+            borderColor: 'rgba(84, 0, 24, 1)',
+            borderWidth: 1
+        }, 
+        {
+            label: '> 80$',
+            backgroundColor: 'rgba(51, 0, 15, 0.8)',
+            hoverBackgroundColor: 'rgba(255, 206, 86, 1)',
+            stack: 'Stack 2',
+            data: [ARG_HIGH, USA_HIGH, SA_HIGH, CH_HIGH],
+            borderColor: 'rgba(51, 0, 15, 1)',
+            borderWidth: 1
+        },
+
+      ]  
+    },
+    options: {
+        legend : {
+            display: true,
+            position: 'bottom',
+        },
+        scales: {
+          xAxes: [{
+            stacked: true
+        }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Ratings'
+                  },
+                stacked: true,
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+    }
+});
+
+$("#changeDatas").on("click", evt => {
+    console.log("yes");
+})
+
